7635cd3c2ce5a4aa73e33103d88c0998860c9bb7
Matt Ware <mattbware3@gmail.com>
2024-11-19 03:54:29 +0000

    Extra rect functions

--- PAPI/Include/Core/Utility/MathUtil.h
+++ PAPI/Include/Core/Utility/MathUtil.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 
-template<typename T = float>
-requires std::is_arithmetic_v<T>
+template <typename T = float>
+	requires std::is_arithmetic_v<T>
 struct TRect
 {
 	glm::vec<2, T> Position;
@@ -9,21 +9,37 @@ struct TRect
 
 	TRect(glm::vec<2, T> position, glm::vec<2, T> size)
 		: Position(position), Size(size)
-	{}
+	{
+	}
 
 	TRect(T x, T y, T width, T height)
 		: Position(glm::vec<2, T>(x, y)), Size(glm::vec<2, T>(width, height))
-	{}
-	
-	NODISCARD FORCEINLINE bool OverlapsWith(const TRect& other)
 	{
-		return Position.x < other.Position.x + other.Size.x &&  // Not to the right of the other rectangle
-			   Position.x + Size.x > other.Position.x &&      // Not to the left of the other rectangle
-			   Position.y < other.Position.y + other.Size.y &&  // Not below the other rectangle
-			   Position.y + Size.y > other.Position.y;         // Not above the other rectangle
 	}
 
-	NODISCARD FORCEINLINE bool Contains(const glm::vec2& point)
+	NODISCARD FORCEINLINE bool OverlapsWith(const TRect &other)
+	{
+		return Position.x < other.Position.x + other.Size.x && // Not to the right of the other rectangle
+			Position.x + Size.x > other.Position.x &&          // Not to the left of the other rectangle
+			Position.y < other.Position.y + other.Size.y &&    // Not below the other rectangle
+			Position.y + Size.y > other.Position.y;            // Not above the other rectangle
+	}
+	
+	NODISCARD FORCEINLINE bool OverlapsWith(T x, T y, T width, T height)
+	{
+		return Position.x < x + width && // Not to the right of the other rectangle
+			Position.x + Size.x > x &&   // Not to the left of the other rectangle
+			Position.y < y + height &&   // Not below the other rectangle
+			Position.y + Size.y > y;     // Not above the other rectangle
+	}
+
+	NODISCARD FORCEINLINE bool ContainsRect(const TRect &other)
+	{
+		return Position.x <= other.Position.x && Position.x + Size.x >= other.Position.x + other.Size.x
+			&& Position.y <= other.Position.y && Position.y + Size.y >= other.Position.y + other.Size.y;
+	}
+
+	NODISCARD FORCEINLINE bool ContainsPoint(const glm::vec2 &point)
 	{
 		return Position.x <= point.x && Position.x + Size.x >= point.x
 			&& Position.y <= point.y && Position.y + Size.y >= point.y;
@@ -44,14 +60,14 @@ public:
 	{
 		return (a - b) * glm::pow(r, delta) + b;
 	}
-	
+
 	NODISCARD FORCEINLINE static glm::vec2 LerpSmooth(glm::vec2 a, glm::vec2 b, float r, float delta)
 	{
-		return { LerpSmooth(a.x, b.x, r, delta), LerpSmooth(a.y, b.y, r, delta) };
+		return {LerpSmooth(a.x, b.x, r, delta), LerpSmooth(a.y, b.y, r, delta)};
 	}
-	
-	NODISCARD FORCEINLINE static glm::vec3 LerpSmooth(const glm::vec3& a, const glm::vec3& b, float r, float delta)
+
+	NODISCARD FORCEINLINE static glm::vec3 LerpSmooth(const glm::vec3 &a, const glm::vec3 &b, float r, float delta)
 	{
-		return { LerpSmooth(a.x, b.x, r, delta), LerpSmooth(a.y, b.y, r, delta), LerpSmooth(a.z, b.z, r, delta) };
+		return {LerpSmooth(a.x, b.x, r, delta), LerpSmooth(a.y, b.y, r, delta), LerpSmooth(a.z, b.z, r, delta)};
 	}
 };
