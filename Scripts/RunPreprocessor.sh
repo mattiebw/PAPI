ba4d24f9332b93f55a417ae64f771ea33ef27c78
Matt Ware <mattbware3@gmail.com>
2024-11-21 22:01:41 +0000

    Fix script

--- Scripts/RunPreprocessor.sh
+++ Scripts/RunPreprocessor.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo Running preprocessor...
-cd ../Tools/PAPIPreprocessor/bin/Release/net8.0/win-x64/
+cd ../Tools/PAPIPreprocessor/bin/Release/net8.0/linux-x64/
 ./PAPIPreprocessor "../../../../../../"
