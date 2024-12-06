0afd63e7798c86b9011a4197764dd0d632cada07
up2210635 <up2210635@myport.ac.uk>
2024-12-06 15:00:37 +0000

    noise
    just more playing around

--- PAPI/Source/World/ChunkProvider.cpp
+++ PAPI/Source/World/ChunkProvider.cpp
@@ -4,10 +4,13 @@
 
 DefaultChunkProvider::DefaultChunkProvider()
 {
+    srand(time(0));
+
     m_Noise.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
+    m_Noise.SetSeed(rand() % 1024);
     m_Noise.SetFractalOctaves(5);
     m_Noise.SetFrequency(0.2f);
-    m_Noise.SetFractalLacunarity(2.0f);
+    m_Noise.SetFractalLacunarity(0.15f);
     m_Noise.SetFractalGain(0.2f);
     m_Noise.SetFractalType(FastNoiseLite::FractalType_FBm);
 }
@@ -15,38 +18,10 @@ DefaultChunkProvider::DefaultChunkProvider()
 uint16_t DefaultChunkProvider::GetTileAt(int x, int y) const
 {
     float noise = m_Noise.GetNoise(static_cast<float>(x), static_cast<float>(y));
-    if (noise >= 0.08f && noise < 0.3f)
+    if (noise >= 0.08f && noise < 0.25f)
         return TileSets::StoneFloor;
-    if (noise >= 0.3f)
+    if (noise >= 0.25f)
         return TileSets::StoneWall;
     
     return TileSets::Grass;
-}
-
-void Noise_Map()
-{
-    FastNoiseLite noise;
-    noise.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
-    noise.SetFrequency(0.2f);
-    noise.SetFractalLacunarity(2.0f);
-    noise.SetFractalGain(0.5f);
-    noise.SetFractalType(FastNoiseLite::FractalType_FBm);
-
-    std::vector<float> noiseData(64 * 64);
-    int index = 0;
-    int Tile{};
-
-    for (int y = 0; y < 64; y++)
-    {
-        for (int x = 0; x < 64; x++)
-        {
-            noiseData[index++] = noise.GetNoise((float)x, (float)y);
-            if (noise.GetNoise((float)x, (float)y) >= 0.08f && noise.GetNoise((float)x, (float)y) < 0.3f)
-                Tile = 1;
-            else if (noise.GetNoise((float)x, (float)y) >= 0.3f)
-                Tile = 2;
-            else
-                Tile = 0;
-        }
-    }
 }
