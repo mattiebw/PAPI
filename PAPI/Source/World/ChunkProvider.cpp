5c0866ed7e2f53009a9ecd1c4ebad0484cc1e401
up2210635 <up2210635@myport.ac.uk>
2024-12-06 16:39:01 +0000

    Noise
    quess

--- PAPI/Source/World/ChunkProvider.cpp
+++ PAPI/Source/World/ChunkProvider.cpp
@@ -9,7 +9,7 @@ DefaultChunkProvider::DefaultChunkProvider()
     m_Noise.SetNoiseType(FastNoiseLite::NoiseType_Perlin);
     m_Noise.SetSeed(rand() % 1024);
     m_Noise.SetFractalOctaves(5);
-    m_Noise.SetFrequency(0.2f);
+    m_Noise.SetFrequency(0.1f);
     m_Noise.SetFractalLacunarity(0.15f);
     m_Noise.SetFractalGain(0.2f);
     m_Noise.SetFractalType(FastNoiseLite::FractalType_FBm);
@@ -20,8 +20,7 @@ uint16_t DefaultChunkProvider::GetTileAt(int x, int y) const
     float noise = m_Noise.GetNoise(static_cast<float>(x), static_cast<float>(y));
     if (noise >= 0.08f && noise < 0.25f)
         return TileSets::StoneFloor;
-    if (noise >= 0.25f)
+    else if (noise >= 0.25f)
         return TileSets::StoneWall;
-    
     return TileSets::Grass;
 }
